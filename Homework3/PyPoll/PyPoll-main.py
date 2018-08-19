49866bac676cc4e8355be606b9a65c821023791b
xarkl <karlsuaco@gmail.com>
2018-08-18 18:53:31 -0700

    updated PyPoll

--- Homework3/PyPoll/PyPoll-main.py
+++ Homework3/PyPoll/PyPoll-main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+#Import 
+import os
+import csv
+
+#read CSV file
+csvpath = os.path.join('Resources','election_data.csv')
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+print(csvreader)
+
+for row in csvreader:
+    print(row)
